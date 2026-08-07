ef6183f4fe0646e7381eba6ce2f2913152b38ddc
iamankushpatel <241310007@nitdelhi.ac.in>
2026-08-07 13:58:12 +0530

    Learned Api responses

--- src/index.js
+++ src/index.js
@@ -8,7 +8,17 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+
+.then(()=>{
+    app.listen(process.env.PORT ||8000)
+    console.log(`server is running at PORT: ${process.env.PORT}`);
+    
+})
+.catch((err) =>{
+    console.log("mongo db connection failed!!",err);
+    
+})
 
 
 
